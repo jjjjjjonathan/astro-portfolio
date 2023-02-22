0fa579c14277bd3e9f6a3c3803246dd77df543e7
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2023-02-21 21:39:48 -0500

    Add filter for drafts in project collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,6 +3,7 @@ import { z, defineCollection } from "astro:content";
 const projectsCollection = defineCollection({
   schema: z.object({
     title: z.string(),
+    draft: z.boolean(),
   }),
 });
 
