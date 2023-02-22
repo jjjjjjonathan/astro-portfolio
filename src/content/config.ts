cb23ac7521651bfbeba7591b0dacbd3d1c5c7fa6
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2023-02-22 12:19:33 -0500

    Add frontmatter props for github and deploy urls

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,6 +3,8 @@ import { z, defineCollection } from "astro:content";
 const projectsCollection = defineCollection({
   schema: z.object({
     title: z.string(),
+    githubUrl: z.string().url(),
+    deployUrl: z.string().url().optional(),
     draft: z.boolean(),
   }),
 });
