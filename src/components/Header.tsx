80cb6b4b47f086e7569f3c59e2af03e90723e2c2
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2023-02-23 19:36:20 -0500

    Update header component to use React

---
+++ src/components/Header.tsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+
+const Header = () => {
+  const [secondaryRole, setSecondaryRole] = useState('a sports writer');
+
+  const roles = [
+    'a sports writer',
+    'a former football coach',
+    'a keyboard builder',
+  ];
+
+  const setNewRole = () => {
+    const currentIndex = roles.indexOf(secondaryRole);
+    let newIndex = -1;
+
+    do {
+      newIndex = Math.floor(Math.random() * roles.length);
+    } while (currentIndex === newIndex);
+    setSecondaryRole(roles[newIndex]);
+  };
+
+  return (
+    <header className='flex h-screen w-full flex-col items-center justify-center gap-12 sm:flex-row sm:gap-24'>
+      <div className='flex flex-col items-center justify-center sm:items-start'>
+        <h1 className='text-4xl font-bold sm:text-5xl'>Hi! I'm Jonathan.</h1>
+        <div className='text-2xl sm:text-3xl'>I'm a full-stack developer</div>
+        <div className='text-2xl sm:text-3xl'>
+          and <span>{secondaryRole}.</span>
+        </div>
+        <button className='rounded-full bg-blue-400 p-3' onClick={setNewRole}>
+          What else am I?
+        </button>
+      </div>
+      <div className='h-48 w-48 bg-black'></div>
+    </header>
+  );
+};
+
+export default Header;
