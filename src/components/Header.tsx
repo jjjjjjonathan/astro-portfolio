60e1890c463e8b2682e9c150ad37af6a3beabe6b
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2023-02-28 15:39:57 -0500

    Add chevron to header and start about section

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -30,39 +30,53 @@ const Header = () => {
   };
 
   return (
-    <div className='flex min-h-screen w-full flex-col items-center justify-center gap-12 md:flex-row md:gap-24'>
-      <div className='flex flex-col items-center justify-center gap-1 md:items-start'>
-        <h1 className='text-4xl font-bold sm:text-5xl'>Hi! I'm Jonathan.</h1>
-        <div className='text-2xl sm:text-3xl'>I'm a full-stack developer</div>
-        <div className='text-2xl sm:text-3xl'>
-          and <span>{secondaryRole.title}.</span>
-        </div>
-        <div className='mt-2 flex w-full flex-row items-center justify-around'>
-          <MdOutlineEmail title='email' className='text-4xl' />
+    <header className='flex min-h-screen w-full flex-col'>
+      <div className='my-auto flex w-full flex-col items-center justify-center gap-12 md:flex-row md:gap-24'>
+        <div className='flex flex-col items-center justify-center gap-1 md:items-start'>
+          <h1 className='text-4xl font-bold sm:text-5xl'>Hi! I'm Jonathan.</h1>
+          <div className='text-2xl sm:text-3xl'>I'm a full-stack developer</div>
+          <div className='text-2xl sm:text-3xl'>
+            and <span>{secondaryRole.title}.</span>
+          </div>
+          <div className='mt-2 flex w-full flex-row items-center justify-around'>
+            {/* <MdOutlineEmail title='email' className='text-4xl' />
           <SiGithub className='text-4xl' title='GitHub' />
-          <SiLinkedin className='text-4xl' title='LinkedIn' />
-        </div>
-      </div>
-      <div className='flex flex-col'>
-        <div className='flex flex-col justify-center shadow-xl'>
-          <div className='h-56 w-48 -rotate-6 bg-neutral-300 pt-4 shadow-xl md:h-96 md:w-72'></div>
-          <div className='-mt-56 h-56 w-48 rotate-12 bg-neutral-200 pt-4 shadow-xl md:-mt-96 md:h-96 md:w-72'></div>
-          <div className='-mt-56 h-56 w-48 rotate-2 bg-neutral-100 pt-4 shadow-xl md:-mt-96 md:h-96 md:w-72'>
-            <img
-              src={secondaryRole.photo}
-              alt=''
-              className='mx-auto h-2/3 sm:h-3/4'
-            />
+          <SiLinkedin className='text-4xl' title='LinkedIn' /> */}
           </div>
         </div>
-        <button
+        <div className='flex flex-col' onClick={setNewRole}>
+          <div className='flex flex-col justify-center shadow-xl'>
+            <div className='h-56 w-48 -rotate-6 bg-neutral-300 pt-4 shadow-xl md:h-96 md:w-72'></div>
+            <div className='-mt-56 h-56 w-48 rotate-12 bg-neutral-200 pt-4 shadow-xl md:-mt-96 md:h-96 md:w-72'></div>
+            <div className='-mt-56 h-56 w-48 rotate-2 bg-neutral-100 pt-4 shadow-xl md:-mt-96 md:h-96 md:w-72'>
+              <img
+                src={secondaryRole.photo}
+                alt=''
+                className='mx-auto h-2/3 sm:h-3/4'
+              />
+            </div>
+          </div>
+          {/* <button
           className='mt-12 block rounded-full bg-emerald-400 p-3 shadow-xl'
           onClick={setNewRole}
         >
           See what else I am!
-        </button>
+        </button> */}
+        </div>
       </div>
-    </div>
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        width='1em'
+        height='1em'
+        viewBox='0 0 24 24'
+        className='mx-auto mb-8 text-4xl lg:text-6xl'
+      >
+        <path
+          fill='currentColor'
+          d='M16.59 5.59L18 7l-6 6l-6-6l1.41-1.41L12 10.17l4.59-4.58m0 6L18 13l-6 6l-6-6l1.41-1.41L12 16.17l4.59-4.58Z'
+        ></path>
+      </svg>
+    </header>
   );
 };
 
