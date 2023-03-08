6cbfa32ff221178e0f9d61373d9673107189fa29
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2023-03-07 20:52:19 -0500

    Stop using default base styles from astro tailwind

--- astro.config.ts
+++ astro.config.ts
@@ -7,9 +7,13 @@ import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
-import react from "@astrojs/react";
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), react()]
+  integrations: [
+    tailwind({ config: { applyBaseStyles: false } }),
+    mdx(),
+    react(),
+  ],
 });
